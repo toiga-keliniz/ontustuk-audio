daeb25e1c13a0e8c54de0ce824a9f1f009ac8a64
toiga-keliniz <orazalievtemirhan@gmail.com>
2025-03-19 21:27:36 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
+let currentAudio = null;
+
 function toggleAudio(audioId) {
     const audio = document.getElementById(audioId);
     const playButton = document.getElementById('play' + audioId.slice(5));
     const progress = document.getElementById('progress' + audioId.slice(5));
     const timeDisplay = document.getElementById('time' + audioId.slice(5));
 
+    // Останавливаем текущее аудио, если оно есть и не совпадает с выбранным
+    if (currentAudio && currentAudio !== audio) {
+        currentAudio.pause();
+        const currentPlayButton = document.getElementById('play' + currentAudio.id.slice(5));
+        currentPlayButton.innerHTML = '<i class="fas fa-play"></i>';
+    }
+
     if (audio.paused) {
         audio.play();
         playButton.innerHTML = '<i class="fas fa-pause"></i>';
+        currentAudio = audio; // Обновляем текущее аудио
     } else {
         audio.pause();
         playButton.innerHTML = '<i class="fas fa-play"></i>';
+        currentAudio = null; // Если остановлено, сбрасываем текущее аудио
     }
 
     audio.addEventListener('timeupdate', function() {
